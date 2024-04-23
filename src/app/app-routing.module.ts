a01df5dfce3f0333e1294ccf32decd35a11e7e56
J.M.Hadjimi <jose.galdon12@gmail.com>
2024-04-23 10:47:49 +0200

    Segundo commit de mantenimiento-de-usuarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {InicioComponent} from "./components/inicio/inicio.component";
+import {UsuariosComponent} from "./components/usuarios/usuarios.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: "full",
+    redirectTo:'/inicio'
+  },
+  {
+    path:'inicio',
+    component: InicioComponent
+  },
+  {
+    path:'usuarios',
+    component: UsuariosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layouts/navbar/navbar.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { UserComponent } from './components/entities/user/user.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    InicioComponent,
+    UserComponent,
+    UsuariosComponent,
   ],
   imports: [
     BrowserModule,
